a2042623e78bf221c375025074f19c92532c72e1
solutionshub-reporting <admin@momentumglobal.co>
2026-05-24 00:40:14 +0100

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,63 @@
+const msalConfig = {
+  auth: {
+    clientId:    CONFIG.CLIENT_ID,
+    authority:   CONFIG.AUTHORITY,
+    redirectUri: CONFIG.REDIRECT_URI,
+  },
+  cache: {
+    cacheLocation: 'sessionStorage',
+    storeAuthStateInCookie: false,
+  },
+};
+
+const msalInstance = new msal.PublicClientApplication(msalConfig);
+
+const loginRequest = {
+  scopes: ['User.Read', 'Sites.ReadWrite.All'],
+};
+
+async function signIn() {
+  try {
+    await msalInstance.loginPopup(loginRequest);
+    const account = msalInstance.getAllAccounts()[0];
+    if (account) {
+      sessionStorage.setItem('userEmail', account.username.toLowerCase());
+      sessionStorage.setItem('userName',  account.name);
+      window.APP.init();
+    }
+  } catch (e) {
+    console.error('Sign-in error:', e);
+  }
+}
+
+function signOut() {
+  msalInstance.logoutPopup();
+  sessionStorage.clear();
+  window.location.reload();
+}
+
+async function getToken() {
+  const account = msalInstance.getAllAccounts()[0];
+  if (!account) return null;
+  try {
+    const response = await msalInstance.acquireTokenSilent({
+      ...loginRequest,
+      account,
+    });
+    return response.accessToken;
+  } catch (e) {
+    const response = await msalInstance.acquireTokenPopup(loginRequest);
+    return response.accessToken;
+  }
+}
+
+function getCurrentUser() {
+  return {
+    email: sessionStorage.getItem('userEmail'),
+    name:  sessionStorage.getItem('userName'),
+  };
+}
+
+function isSignedIn() {
+  return msalInstance.getAllAccounts().length > 0;
+}

47403a1cead4be81792dd85d7cd6c97469aed1cf
momentumglobal <admin@momentumglobal.co>
2026-07-28 13:21:55 +0100

    Update lci-sections.js - legacy table format fix

--- js/lci-sections.js
+++ js/lci-sections.js
@@ -63,6 +63,7 @@ function _lciLegacyHtml() {
                    onchange="lciRowFieldChanged(${gidx}, 'Team', this.value)"></td>
         <td><select class="lci-cell" onchange="lciRowFieldChanged(${gidx}, 'LegacyCategory', this.value)">${catOpts}</select></td>
         <td><input type="number" class="lci-cell lci-cell--sm" min="1" value="${r.Quantity ?? 1}"
+                   onchange="lciRowFieldChanged(${gidx}, 'Quantity', this.value)"></td>
         <td><input type="number" class="lci-cell" min="0" value="${r.AnnualSalary ?? ''}"
                    onchange="lciRowFieldChanged(${gidx}, 'AnnualSalary', this.value)"></td>
         <td><input type="number" class="lci-cell lci-cell--sm" min="0" max="100" step="1" value="${r.BonusPct != null ? Math.round(r.BonusPct * 100 * 100) / 100 : ''}"
